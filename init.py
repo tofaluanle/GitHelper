0f964f4a834583fe1a3e8d3fa3b05fd8a6a82610
sjj@work <songjiangjiang@sina.com>
2017-12-12 16:29:21 +0800

    init脚本增加更新manifest逻辑

--- init.py
+++ init.py
@@ -5,22 +5,52 @@ import Util
 import subprocess
 import shutil
 import sys
+import os
 
+PATH = os.getcwd() + '/.githelper/manifest'
 
 def main(manifestUri):
-    shutil.copytree(sys.path[0], '.githelper/python')
+    # shutil.copytree(sys.path[0], '.githelper/python')
 
-    ret = clone(manifestUri)
-    if ret != 0:
-        raise Exception('manifest clone ' + manifestUri + ' fail: ' + str(ret))
+    if os.path.exists(PATH):
+        ret = update()
+        if ret != 0:
+            raise Exception('manifest update ' + manifestUri + ' fail: ' + str(ret))
 
-    Util.cprint('manifest clone ' + manifestUri + ' success')
+        Util.cprint('manifest update ' + manifestUri + ' success')
+
+        ret = reset()
+        if ret != 0:
+            raise Exception('manifest reset ' + manifestUri + ' fail: ' + str(ret))
+
+        Util.cprint('manifest reset ' + manifestUri + ' success')
+        
+    else:
+        ret = clone(manifestUri)
+        if ret != 0:
+            raise Exception('manifest clone ' + manifestUri + ' fail: ' + str(ret))
+    
+        Util.cprint('manifest clone ' + manifestUri + ' success')
 
     return 0
 
 
 def clone(git):
     obj = subprocess.Popen(['git', 'clone', git, '.githelper/manifest'], stdout=subprocess.PIPE)
+    obj.wait()
+    ret = obj.returncode
+    return ret
+
+
+def update():
+    obj = subprocess.Popen(['git', 'remote', 'update'], stdout=subprocess.PIPE, cwd=PATH)
+    obj.wait()
+    ret = obj.returncode
+    return ret
+
+
+def reset():
+    obj = subprocess.Popen(['git', 'reset', '--hard', 'origin/master'], stdout=subprocess.PIPE, cwd=PATH)
     obj.wait()
     ret = obj.returncode
     return ret
